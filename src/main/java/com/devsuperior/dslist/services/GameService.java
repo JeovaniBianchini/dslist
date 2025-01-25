58ee10620f976772dd08edc4cc8104904f17656b
Jeovani Bianchini <bianchini.jeovani@gmail.com>
2025-01-25 11:35:06 -0400

    Projection, SQL,  Get find games by list

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dslist.services;
 import com.devsuperior.dslist.dto.GameDto;
 import com.devsuperior.dslist.dto.GameMinDto;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repository.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,5 +28,11 @@ public class GameService {
         List<Game> newList = gameRepository.findAll();
         List<GameMinDto> dto = newList.stream().map(x -> new GameMinDto(x)).toList();
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDto> findByList(Long listId){
+        List<GameMinProjection> newList = gameRepository.searchByList(listId);
+        return newList.stream().map(x -> new GameMinDto(x)).toList();
     }
 }
